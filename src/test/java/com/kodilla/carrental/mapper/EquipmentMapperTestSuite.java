8f2b4d9e80d7639f25dfacf31e9a597736a27c08
lukgrz <lukaszgrzegorzak@gmail.com>
2022-01-11 13:53:22 +0100

    Equipment mapper tests

---
+++ src/test/java/com/kodilla/carrental/mapper/EquipmentMapperTestSuite.java
@@ -0,0 +1,76 @@
+package com.kodilla.carrental.mapper;
+
+import com.kodilla.carrental.domain.Client;
+import com.kodilla.carrental.domain.Equipment;
+import com.kodilla.carrental.domain.Price;
+import com.kodilla.carrental.dto.ClientDto;
+import com.kodilla.carrental.dto.EquipmentDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class EquipmentMapperTestSuite {
+
+    @Autowired
+    private EquipmentMapper equipmentMapper;
+
+    @Test
+    public void mapToEquipment() {
+        //Given
+        EquipmentDto equipmentDto = new EquipmentDto(1l,"Baby seat", "Seat for baby",
+                new Price(new BigDecimal(20)));
+        //When
+        Equipment equipment = equipmentMapper.mapToEquipment(equipmentDto);
+        //Then
+        assertEquals(1l, equipment.getId());
+        assertEquals("Baby seat", equipment.getName());
+        assertEquals("Seat for baby", equipment.getDescription());
+    }
+
+    @Test
+    public void mapToEquipmentDto() {
+        //Given
+        Equipment equipment = new Equipment(1l,"Baby seat", "Seat for baby",
+                new Price(new BigDecimal(20)));
+        //When
+        EquipmentDto equipmentDto = equipmentMapper.mapToEquipmentDto(equipment);
+        //Then
+        assertEquals(1l, equipmentDto.getId());
+        assertEquals("Baby seat", equipmentDto.getName());
+        assertEquals("Seat for baby", equipmentDto.getDescription());
+    }
+
+    @Test
+    public void mapToEquipmentDtoList () {
+        //Given
+        Equipment equipment1 = new Equipment(1l,"Baby seat", "Seat for baby",
+                new Price(new BigDecimal(20)));
+        Equipment equipment2 = new Equipment(2l,"Trailer", "Small trailer",
+                new Price(new BigDecimal(40)));
+        List<Equipment> equipmentList = List.of(equipment1,equipment2);
+        //When
+        List<EquipmentDto> equipmentDtoList = equipmentMapper.mapToEquipmentDtoList(equipmentList);
+        //Then
+        assertNotNull(equipmentDtoList);
+        assertTrue(equipmentDtoList.size() == 2);
+        assertEquals("Trailer", equipmentDtoList.get(1).getName());
+    }
+
+    @Test
+    public void testMapToEquipmentDtoEmptyList() {
+        //Given
+        List<Equipment> equipmentList = List.of();
+        //When
+        List<EquipmentDto> equipmentDtoList = equipmentMapper.mapToEquipmentDtoList(equipmentList);
+        //Then
+        assertNotNull(equipmentDtoList);
+        assertTrue(equipmentDtoList.size() == 0);
+    }
+}
