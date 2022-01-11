eb3da651df767ef69c789b274d5659d7f301ca6d
lukgrz <lukaszgrzegorzak@gmail.com>
2022-01-11 12:08:01 +0100

    Rent Mapper test suite

---
+++ src/test/java/com/kodilla/carrental/mapper/RentMapperTestSuite.java
@@ -0,0 +1,98 @@
+package com.kodilla.carrental.mapper;
+
+import com.kodilla.carrental.domain.*;
+import com.kodilla.carrental.dto.RentDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class RentMapperTestSuite {
+
+    @Autowired
+    private RentMapper rentMapper;
+
+    @Test
+    public void testMapToRent() {
+        //Given
+        RentDto rentDto = new RentDto(1l, LocalDate.of(2021, 1, 1),
+                LocalDate.of(2021, 1, 5), List.of(new Equipment()), new Car(), new Client(), new Price());
+        //When
+        Rent rent = rentMapper.mapToRent(rentDto);
+        //Then
+        assertTrue(1L == rent.getId());
+        assertEquals(5, rent.numberOfDaysForRent());
+    }
+
+    @Test
+    public void testMapToRentDto() {
+        //Given
+        Rent rent = new Rent(1l, LocalDate.of(2021, 1, 1),
+                LocalDate.of(2021, 1, 5), List.of(new Equipment()), new Car(), new Client(), new Price());
+        //When
+        RentDto rentDto = rentMapper.mapToRentDto(rent);
+        //Then
+        assertTrue(1L == rentDto.getId());
+        assertEquals(5, rentDto.getReturnDate().getDayOfMonth());
+    }
+
+    @Test
+    public void testMapToRentDtoList() {
+        //Given
+        Rent rent1 = new Rent(1l, LocalDate.of(2021, 1, 1),
+                LocalDate.of(2021, 1, 5), List.of(new Equipment()), new Car(), new Client(), new Price());
+        Rent rent2 = new Rent(2l, LocalDate.of(2021, 1, 1),
+                LocalDate.of(2021, 1, 5), List.of(new Equipment()), new Car(), new Client(), new Price());
+        List<Rent> rents = List.of(rent1, rent2);
+        //When
+        List<RentDto> rentDtos = rentMapper.mapToRentDtoList(rents);
+        //Then
+        assertNotNull(rentDtos);
+        assertTrue(rentDtos.size() == 2);
+        assertEquals(2l, rentDtos.get(1).getId());
+    }
+
+    @Test
+    public void testMapToRentDtoEmptyList() {
+        //Given
+        List<Rent> rents = List.of();
+        //When
+        List<RentDto> rentDtos = rentMapper.mapToRentDtoList(rents);
+        //Then
+        assertNotNull(rentDtos);
+        assertTrue(rentDtos.size() == 0);
+    }
+
+
+    @Test
+    public void testMapToRentList() {
+        //Given
+        RentDto rent1 = new RentDto(1l, LocalDate.of(2021, 1, 1),
+                LocalDate.of(2021, 1, 5), List.of(new Equipment()), new Car(), new Client(), new Price());
+        RentDto rent2 = new RentDto(2l, LocalDate.of(2021, 1, 1),
+                LocalDate.of(2021, 1, 5), List.of(new Equipment()), new Car(), new Client(), new Price());
+        List<RentDto> rentDtos = List.of(rent1, rent2);
+        //When
+        List<Rent> rents = rentMapper.mapToRentList(rentDtos);
+        //Then
+        assertNotNull(rentDtos);
+        assertTrue(rentDtos.size() == 2);
+        assertEquals(2l, rentDtos.get(1).getId());
+    }
+
+    @Test
+    public void testMapToRentEmptyList() {
+        //Given
+        List<RentDto> rentDtos = List.of();
+        //When
+        List<Rent> rents = rentMapper.mapToRentList(rentDtos);
+        //Then
+        assertNotNull(rentDtos);
+        assertTrue(rentDtos.size() == 0);
+    }
+}
