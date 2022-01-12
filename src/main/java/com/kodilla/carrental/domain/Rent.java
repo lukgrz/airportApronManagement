884150ad57c2d8e63c1e64f1f128df6b98c8376d
lukgrz <lukaszgrzegorzak@gmail.com>
2022-01-12 16:13:51 +0100

    updated rent service

--- src/main/java/com/kodilla/carrental/domain/Rent.java
+++ src/main/java/com/kodilla/carrental/domain/Rent.java
@@ -68,63 +68,76 @@ public class Rent {
 
     }
 
-    public void calculateTotalPrice (RateService rateService) {
+    public BigDecimal calculateTotalPrice(RateService rateService) {
         Rate rate = rateService.fetchRates();
-        BigDecimal totalPriceInPln = calculateTotalPriceForCar().add(calculateTotalPriceForEquipment());
+        switch (currency) {
+            case PLN:
+                BigDecimal totalPriceInPln = calculateTotalPriceForCar().add(calculateTotalPriceForEquipment());
+                totalPrice = totalPriceInPln;
+            case USD:
+                BigDecimal totalPriceInUsd = calculateTotalPriceForCar().add(calculateTotalPriceForEquipment())
+                        .multiply(BigDecimal.valueOf(rate.getPln_Usd()));
+                totalPrice = totalPriceInUsd;
+            case EUR:
+                BigDecimal totalPriceInEur = calculateTotalPriceForCar().add(calculateTotalPriceForEquipment())
+                        .multiply(BigDecimal.valueOf(rate.getPln_Eur()));
+                totalPrice = totalPriceInEur;
+        }
+        return totalPrice;
     }
 
-    public static class RentBuilder {
-        private Long id;
-        private LocalDate rentDate;
-        private LocalDate returnDate;
-        private List<Equipment> equipmentList = new ArrayList<>();
-        private Car car;
-        private Client client;
-        private Currency currency;
-        private BigDecimal totalPrice;
+        public static class RentBuilder {
+            private Long id;
+            private LocalDate rentDate;
+            private LocalDate returnDate;
+            private List<Equipment> equipmentList = new ArrayList<>();
+            private Car car;
+            private Client client;
+            private Currency currency;
+            private BigDecimal totalPrice;
 
-        public RentBuilder id(Long id) {
-            this.id = id;
-            return this;
-        }
+            public RentBuilder id(Long id) {
+                this.id = id;
+                return this;
+            }
 
-        public RentBuilder rentDate(LocalDate rentDate) {
-            this.rentDate = rentDate;
-            return this;
-        }
+            public RentBuilder rentDate(LocalDate rentDate) {
+                this.rentDate = rentDate;
+                return this;
+            }
 
-        public RentBuilder returnDate(LocalDate returnDate) {
-            this.returnDate = returnDate;
-            return this;
-        }
+            public RentBuilder returnDate(LocalDate returnDate) {
+                this.returnDate = returnDate;
+                return this;
+            }
 
-        public RentBuilder equipment(Equipment equipment) {
-            equipmentList.add(equipment);
-            return this;
-        }
+            public RentBuilder equipment(Equipment equipment) {
+                equipmentList.add(equipment);
+                return this;
+            }
 
-        public RentBuilder car(Car car) {
-            this.car = car;
-            return this;
-        }
+            public RentBuilder car(Car car) {
+                this.car = car;
+                return this;
+            }
 
-        public RentBuilder client(Client client) {
-            this.client = client;
-            return this;
-        }
+            public RentBuilder client(Client client) {
+                this.client = client;
+                return this;
+            }
 
-        public RentBuilder currency(Currency currency) {
-            this.currency = currency;
-            return this;
-        }
+            public RentBuilder currency(Currency currency) {
+                this.currency = currency;
+                return this;
+            }
 
-        public RentBuilder totalPrice(BigDecimal totalPrice) {
-            this.totalPrice = totalPrice;
-            return this;
-        }
+            public RentBuilder totalPrice(BigDecimal totalPrice) {
+                this.totalPrice = totalPrice;
+                return this;
+            }
 
-        public Rent build() {
-            return new Rent(id, rentDate, returnDate, equipmentList, car, client, currency, totalPrice);
+            public Rent build() {
+                return new Rent(id, rentDate, returnDate, equipmentList, car, client, currency, totalPrice);
+            }
         }
     }
-}
